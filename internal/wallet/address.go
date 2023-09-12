c35942882d024dc9fa7b7c793425ffb6baca24c8
manu <manuel.adalidmoya@jobtome.com>
2023-09-12 15:02:39 +0200

    create wallet with address

---
+++ internal/wallet/address.go
@@ -0,0 +1,48 @@
+package wallet
+
+import (
+	"crypto/ecdsa"
+	"crypto/sha256"
+	"github.com/btcsuite/btcutil/base58"
+	"golang.org/x/crypto/ripemd160"
+)
+
+// generateAddress generates a blockchain wallet address based on an ecdsa public key
+func generateAddress(pKey *ecdsa.PublicKey) string {
+	// SHA-256 hashing on the wallet public key
+	h2 := sha256.New()
+	h2.Write(pKey.X.Bytes())
+	h2.Write(pKey.Y.Bytes())
+	digest2 := h2.Sum(nil)
+
+	// RIPEMD-160 on the SHA-256 hash
+	h3 := ripemd160.New()
+	h3.Write(digest2)
+	digest3 := h3.Sum(nil)
+
+	// Add version byte in front of the RIPEMD-160 hash
+	vd4 := make([]byte, 21)
+	vd4[0] = 0x0
+	copy(vd4[1:], digest3)
+
+	// SHA-256 on the byte prepended RIPEMD-160 hash
+	h5 := sha256.New()
+	h5.Write(vd4)
+	digest5 := h5.Sum(nil)
+
+	// SHA-256 on the last SHA-256 hash
+	h6 := sha256.New()
+	h6.Write(digest5)
+	digest6 := h6.Sum(nil)
+
+	// Take the 4 first bytes of the last SHA-256 hash as checksum
+	checksum := digest6[:4]
+
+	// Join the checksum and the byte prepended RIPEMD-160 hash
+	dc8 := make([]byte, 25)
+	copy(dc8[21:], checksum)
+	copy(dc8[:21], vd4)
+
+	// Encode result to base58 as the wallet address
+	return base58.Encode(dc8)
+}
---
+++ internal/wallet/wallet.go
@@ -0,0 +1,34 @@
+package wallet
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"fmt"
+)
+
+type Wallet struct {
+	key     *ecdsa.PrivateKey
+	address string
+}
+
+func New() (*Wallet, error) {
+	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return nil, fmt.Errorf("could not generate ecdsa key: %w", err)
+	}
+
+	return &Wallet{key: key, address: generateAddress(&key.PublicKey)}, nil
+}
+
+func (w *Wallet) PrivateKey() *ecdsa.PrivateKey {
+	return w.key
+}
+
+func (w *Wallet) PublicKey() *ecdsa.PublicKey {
+	return &w.key.PublicKey
+}
+
+func (w *Wallet) Address() string {
+	return w.address
+}
